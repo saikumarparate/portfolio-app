9d08b64c25335d559dcc49e23081d877c035701b
saikumarparate <sparate@unifocus.com>
2023-10-15 21:37:19 +0530

    corrected the json file name

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { TemplateLean } from '@allamgr/portafolio'
-import jsonData from './components/app.json';
+import jsonData from './components/data.json';
 
 function App() {
 
